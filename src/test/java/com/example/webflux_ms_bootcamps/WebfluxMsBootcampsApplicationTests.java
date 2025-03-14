ccab1aa73a0efd209abc4ae0b94ec6e029fef8b6
SayiraQuirama <sayira.quirama@pragma.com.co>
2025-03-14 08:34:49 -0500

    Initial commit

---
+++ src/test/java/com/example/webflux_ms_bootcamps/WebfluxMsBootcampsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webflux_ms_bootcamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxMsBootcampsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
